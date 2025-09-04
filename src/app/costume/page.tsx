f4fcc06ec711f8ff97470a33080af6a6345ce9ab
WhiteDog1004 <dlrlgus1204@naver.com>
2025-09-04 19:45:41 +0900

    refac : metadata 추가 반영

--- src/app/costume/page.tsx
+++ src/app/costume/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import { getDetailList } from "@/src/features/costume/model/actions";
 import { CostumeList } from "@/src/modules/costume/ui/CostumeList";
+import { SITE_METADATA } from "@/src/shared/lib/sitemap";
 import { Box } from "@/src/shared/ui/box";
+
+export const metadata: Metadata = {
+	...SITE_METADATA.costume,
+};
 
 const CostumePage = async () => {
 	const data = await getDetailList();
