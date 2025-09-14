7276eb54075c14ca41c0af3bd3addb6c08c9d6ec
WhiteDog1004 <dlrlgus1204@naver.com>
2025-09-14 14:30:11 +0900

    refac : 없는 아이템 검색 시 레이아웃 깨지는 현상 fix

--- src/features/artifact/ui/ArtifactList.tsx
+++ src/features/artifact/ui/ArtifactList.tsx
@@ -14,6 +14,7 @@ export const ArtifactList = ({ data }: ArtifactProps) => {
 	const { theme } = useTheme();
 
 	const [mounted, setMounted] = useState(false);
+	const resultData = data.filter((item) => !item.disabled);
 
 	useEffect(() => {
 		setMounted(true);
@@ -22,11 +23,10 @@ export const ArtifactList = ({ data }: ArtifactProps) => {
 	if (!mounted) return;
 	return (
 		<Box
-			className={`grid max-w-3xl max-h-[calc(100svh-360px)] overflow-auto h-full p-4 rounded-lg ${clsx(data.length > 0 ? "grid-cols-[repeat(auto-fill,minmax(80px,1fr))]" : "grid-cols-1", theme === "light" ? "bg-gray-200" : "bg-[#40273b]")}`}
+			className={`grid max-w-3xl max-h-[calc(100svh-360px)] overflow-auto h-full p-4 rounded-lg ${clsx(resultData.length > 0 ? "grid-cols-[repeat(auto-fill,minmax(80px,1fr))]" : "grid-cols-1", theme === "light" ? "bg-gray-200" : "bg-[#40273b]")}`}
 		>
-			{data.length > 0 ? (
-				data
-					.filter((item) => !item.disabled)
+			{resultData.length > 0 ? (
+				resultData
 					.sort(
 						(a, b) =>
 							getRarityValue(a.tier as Rarity) -
