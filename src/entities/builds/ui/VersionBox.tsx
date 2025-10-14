8547d60cee07fb70698b83fa78abe8a061c0909c
WhiteDog1004 <dlrlgus1204@naver.com>
2025-10-14 23:59:31 +0900

    design : version 문구 수정

--- src/entities/builds/ui/VersionBox.tsx
+++ src/entities/builds/ui/VersionBox.tsx
@@ -3,13 +3,21 @@ import { Row, Typography } from "@/src/shared";
 
 export const VersionBox = ({ version }: { version: string }) => {
 	return (
-		<Row className="items-center">
+		<Row className="items-center w-max gap-1">
 			<Typography
 				variant="caption"
 				className={`w-max whitespace-nowrap ${clsx(process.env.NEXT_PUBLIC_GAME_VERSION === version ? "text-yellow-700 dark:text-yellow-500" : "text-gray-400 dark:text-gray-600")}`}
 			>
-				버전: {version}
+				{process.env.NEXT_PUBLIC_GAME_VERSION === version && "버전:"} {version}
 			</Typography>
+			{process.env.NEXT_PUBLIC_GAME_VERSION !== version && (
+				<Typography
+					variant="caption"
+					className="w-max text-gray-400 dark:text-gray-600"
+				>
+					(이전버전)
+				</Typography>
+			)}
 		</Row>
 	);
 };
