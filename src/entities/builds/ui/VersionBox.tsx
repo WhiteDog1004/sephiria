e4f43c56821694346178d8351b526571d4fa5465
WhiteDog1004 <dlrlgus1204@naver.com>
2025-10-26 05:44:43 +0900

    design : 비슷한 버전이면 active color로 보이도록 변경

--- src/entities/builds/ui/VersionBox.tsx
+++ src/entities/builds/ui/VersionBox.tsx
@@ -2,11 +2,22 @@ import clsx from "clsx";
 import { Row, Typography } from "@/src/shared";
 
 export const VersionBox = ({ version }: { version: string }) => {
+	const currentVersion = process.env.NEXT_PUBLIC_GAME_VERSION ?? "0.0.0";
+	const currentMajorMinor = currentVersion.split(".").slice(0, 2).join(".");
+	const targetMajorMinor = version.split(".").slice(0, 2).join(".");
+
+	const isSameVersion = currentMajorMinor === targetMajorMinor;
+
 	return (
 		<Row className="items-center w-max gap-1">
 			<Typography
 				variant="caption"
-				className={`w-max whitespace-nowrap ${clsx(process.env.NEXT_PUBLIC_GAME_VERSION === version ? "text-yellow-600/70 dark:text-yellow-500/70" : "text-gray-400 dark:text-gray-600")}`}
+				className={clsx(
+					"w-max whitespace-nowrap",
+					isSameVersion
+						? "text-yellow-600/70 dark:text-yellow-500/70"
+						: "text-gray-400 dark:text-gray-600",
+				)}
 			>
 				버전: {version}
 			</Typography>
