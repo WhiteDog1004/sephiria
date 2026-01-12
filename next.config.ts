5ea0f647ec98d562d11608e619a1c78444f68944
WhiteDog1004 <dlrlgus1204@naver.com>
2026-01-12 23:17:54 +0900

    refac : public 이미지 cache 반영

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,15 @@ const nextConfig: NextConfig = {
 					},
 				],
 			},
+			{
+				source: "/:path*(.jpg|.jpeg|.png|.gif|.svg|.webp|.ico|.woff|.woff2)",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+				],
+			},
 		];
 	},
 
