3b26087725bf02556cc0c2c05d2558ed444a34c3
WhiteDog1004 <dlrlgus1204@naver.com>
2026-02-26 21:59:35 +0900

    refac : SEO 수정

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -3,6 +3,21 @@ import { NextResponse } from "next/server";
 export function GET() {
 	return new NextResponse(
 		`
+User-agent: Googlebot
+Allow: /
+
+User-agent: Bingbot
+Allow: /
+
+User-agent: Naverbot
+Allow: /
+
+User-agent: SemrushBot
+Disallow: /builds/
+
+User-agent: AhrefsBot
+Disallow: /builds/
+
 User-agent: *
 Allow: /
 Disallow: /fonts/
