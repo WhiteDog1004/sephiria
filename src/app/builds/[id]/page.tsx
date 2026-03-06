9f3ef81df9a231a6369bda46ff55145730f61804
WhiteDog1004 <dlrlgus1204@naver.com>
2026-03-07 04:19:08 +0900

    refac : 빌드 상세 페이지 seo 수정

--- src/app/builds/[id]/page.tsx
+++ src/app/builds/[id]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { cache } from "react";
 import { getBuildDetail } from "@/src/entities/build-detail";
 import { BuildDetailClientPage } from "@/src/modules/build-detail";
+import { COSTUMES } from "@/src/shared";
 
 const getBuildDetailCached = cache(async (id: string) =>
 	getBuildDetail({ id }),
@@ -14,11 +15,18 @@ export async function generateMetadata({
 	params: Promise<{ id: string }>;
 }): Promise<Metadata> {
 	const { id } = await params;
+	const { data } = await getBuildDetailCached(id);
+	if (!data) {
+		return {
+			title: "세피리아 위키",
+			description:
+				"액션 로그라이트 '세피리아(Sephiria)'의 모든 것! 코스튬, 무기, 아티팩트 등 상세한 게임 정보를 제공합니다. 나만의 강력한 빌드를 공유하고 다양한 유저들의 빌드도 확인해 보세요.",
+		};
+	}
 
 	return {
-		title: "빌드 상세 - 세피리아 위키",
-		description:
-			"세피리아 빌드 상세 페이지 - 유저가 직접 등록한 빌드를 확인해 보세요!",
+		title: `${data.title} - 세피리아 위키`,
+		description: `${COSTUMES[data.costume].name} 코스튬 빌드 & v${data.version} - 세피리아 위키`,
 		alternates: {
 			canonical: `/builds/${id}`,
 		},
@@ -27,9 +35,8 @@ export async function generateMetadata({
 			follow: true,
 		},
 		openGraph: {
-			title: "빌드 상세 - 세피리아 위키",
-			description:
-				"세피리아 빌드 상세 페이지 - 유저가 직접 등록한 빌드를 확인해 보세요!",
+			title: `${data.title} - 세피리아 위키`,
+			description: `${COSTUMES[data.costume].name} 코스튬 빌드 & v${data.version} - 세피리아 위키`,
 			images: [
 				{
 					url: "https://sephiria.wiki/thumbnail.png",
